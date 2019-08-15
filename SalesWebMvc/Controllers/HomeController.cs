1d398de1240b20ae42049bccc761c6590208355e
dancesar <danilo@additfiscal.com.br>
2019-08-15 17:13:02 -0300

    First controller and Razor page tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web application from C# course.";
+            ViewData["Email"] = "dancesar@gmail.com";
 
             return View();
         }
